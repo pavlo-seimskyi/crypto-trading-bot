2c8faf1a71db93e369b82688c2959e5a844d31e1
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-07-20 17:51:47 +0200

    document credentials example

--- credentials_example.py
+++ credentials_example.py
@@ -1,13 +1,14 @@
+# Rename this file to credentials.py after you've inserted the credentials
 import config
 
+# Can be created at https://public.sandbox.pro.coinbase.com/profile/api
 if not config.SANDBOX :
-    # Pro
     KEY = '######'
     SECRET = '######'
     PASSPHRASE = '######'
 
+# Can be created at https://pro.coinbase.com/profile/api
 elif config.SANDBOX :
-    # SANDBOX
     KEY = '######'
     SECRET = '######'
     PASSPHRASE = '######'
