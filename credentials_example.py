3c00e293b1221ac096b7611dc6a43efdb49dbb0d
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-07-20 17:48:49 +0200

    add example of cred file

---
+++ credentials_example.py
@@ -0,0 +1,13 @@
+import config
+
+if not config.SANDBOX :
+    # Pro
+    KEY = '######'
+    SECRET = '######'
+    PASSPHRASE = '######'
+
+elif config.SANDBOX :
+    # SANDBOX
+    KEY = '######'
+    SECRET = '######'
+    PASSPHRASE = '######'
