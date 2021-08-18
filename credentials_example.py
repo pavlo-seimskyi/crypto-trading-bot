4d0784ff00d42df372e16b3f247b55ee707ca7ce
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-08-18 19:06:57 +0200

    remove old config

--- credentials_example.py
+++ credentials_example.py
@@ -1,14 +1,14 @@
 # Rename this file to credentials.py after you've inserted the credentials
-import config_coinbase
+import coinbase_config
 
 # Can be created at https://public.sandbox.pro.coinbase.com/profile/api
-if not config_coinbase.SANDBOX :
+if not coinbase_config.SANDBOX :
     KEY = '######'
     SECRET = '######'
     PASSPHRASE = '######'
 
 # Can be created at https://pro.coinbase.com/profile/api
-elif config_coinbase.SANDBOX :
+elif coinbase_config.SANDBOX :
     KEY = '######'
     SECRET = '######'
     PASSPHRASE = '######'
