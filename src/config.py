ec9e4c6ff46451b2cb7004214e14b7c6953a60fd
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-09-17 18:53:12 +0200

    resetting config

--- src/config.py
+++ src/config.py
@@ -1,7 +1,6 @@
 # TO TRADE
 CURRENCY_TO_BUY = 'BTC'
 CURRENCY_TO_SELL = 'EUR'
-SPEND_PER_TRADE = 10.0
 
 # SAVING THE DATA
 ALL_CURRENCIES = ["BTC", "ADA", "LINK", "LTC", "BNB", "ETH", "XRP", "DOGE"]
@@ -32,10 +31,11 @@ SELECTED_TWITTER_PROFILES = [
 # START_DATE = '2019-07-22'
 # END_DATE = '2021-07-24'
 INTERVAL = '1m' # 1m, 5m, 15m, 1h, 2h, 4h, 6h, 8h, 1d, 3d, etc.
-DAYS_BACK = 2 # 2 years back -> 365 * 2
-FOLDER_TO_SAVE = 'test_saving'
+DAYS_BACK = 365 * 2 # 2 years back -> 365 * 2
+FOLDER_TO_SAVE = 'data'
 
 # REAL-TIME DATA
 LATEST_DATA_LOOKBACK_MIN = 20
 
+# Will be the common column in different datasets to merge on
 MERGE_DATA_ON = 'exact_time'
