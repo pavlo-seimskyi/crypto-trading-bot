4575d8d281edcfcde3b2fe2fb8e2d3d9980b2639
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-09-17 17:10:27 +0200

    modifying credentials_example.py to match binance format

--- credentials_example.py
+++ credentials_example.py
@@ -1,14 +1,3 @@
-# Rename this file to credentials.py after you've inserted the credentials
-import coinbase_config
-
-# Can be created at https://public.sandbox.pro.coinbase.com/profile/api
-if not coinbase_config.SANDBOX :
-    KEY = '######'
-    SECRET = '######'
-    PASSPHRASE = '######'
-
-# Can be created at https://pro.coinbase.com/profile/api
-elif coinbase_config.SANDBOX :
-    KEY = '######'
-    SECRET = '######'
-    PASSPHRASE = '######'
+# Insert your Keys here
+BINANCE_API_KEY = '##########'
+BINANCE_API_SECRET = '##########'
