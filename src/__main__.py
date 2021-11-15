7a5af557473069d0a0706d611d9c30252ae32203
Pavlo Seimskyi <pavlo.seimskyi@glovoapp.com>
2021-11-15 18:35:53 +0100

    adding get-data

--- src/__main__.py
+++ src/__main__.py
@@ -8,46 +8,21 @@ import time
 
 
 if __name__ == '__main__':
-    # TESTING NEW STRUCTURE
     # Initializing the timestamps to use in all data scrapers
     end_timestamp = time_helpers.get_current_timestamp()
     training_start_timestamp = time_helpers.get_training_start_timestamp(end_timestamp=end_timestamp)
     production_start_timestamp = time_helpers.get_production_start_timestamp(end_timestamp=end_timestamp)
 
     # Exchange data
-    BinanceScraper = src.data_scraper.scraper_binance.Binance(dev_run=False)
+    # Dev mode
+    BinanceScraper = src.data_scraper.scraper_binance.BinanceScraper(dev_run=True)
+    training_data = BinanceScraper.get_data(training_start_timestamp, end_timestamp)
+    print(training_data.tail())
 
-    # binance_historical_df = BinanceScraper.get_data(
-    #     start_time=training_start_timestamp, end_time=end_timestamp, overwrite=False)
-    # print(binance_historical_df.head())
-
-    binance_latest_df = BinanceScraper.get_data(
-        start_time=production_start_timestamp, end_time=end_timestamp, overwrite=True)
-    print(binance_latest_df.head())
-
-
-    # Twitter Generic Tweets
-    TwitterGenericScraper = scraper_twitter.TwitterGeneric(dev_run=False)
-
-    # historical_generic_tweets = TwitterGenericScraper.get_data(
-    #     start_timestamp=training_start_timestamp, end_timestamp=end_timestamp, save_checkpoint=False, overwrite=True)
-    # print(historical_generic_tweets.tail())
-
-    latest_generic_tweets = TwitterGenericScraper.get_data(
-        start_timestamp=production_start_timestamp, end_timestamp=end_timestamp, save_checkpoint=False, overwrite=True)
-    print(latest_generic_tweets.tail())
-
-
-    # Twitter Profiles with timestamp
-    TwitterProfilesScraper = scraper_twitter.TwitterProfiles(dev_run=False)
-
-    # historical_profile_tweets = TwitterProfilesScraper.get_data(
-    #     start_timestamp=training_start_timestamp, end_timestamp=end_timestamp, save_checkpoint=False, overwrite=True)
-    # print(historical_profile_tweets.tail())
-
-    latest_profile_tweets = TwitterProfilesScraper.get_data(
-        start_timestamp=production_start_timestamp, end_timestamp=end_timestamp, save_checkpoint=False, overwrite=True)
-    print(latest_profile_tweets.tail())
+    # Prod mode
+    BinanceScraper = src.data_scraper.scraper_binance.BinanceScraper(dev_run=False)
+    prod_data = BinanceScraper.get_data(production_start_timestamp, end_timestamp)
+    print(prod_data.tail())
 
 
 
--- src/data_scraper/scraper_binance.py
+++ src/data_scraper/scraper_binance.py
@@ -1,7 +1,10 @@
 import os
 from functools import partial
+from dateutil import parser
 
 import pandas as pd
+import pyarrow.parquet as pq
+import pyarrow as pa
 
 import credentials
 from src import config
@@ -22,66 +25,92 @@ class BinanceScraper:
         all_currencies.remove(self.currency_to_buy)
         self.currency_pairs = [f'{self.currency_to_buy}{self.currency_to_sell}',
                                *[f'{currency}{self.currency_to_sell}' for currency in all_currencies]]
-        self.col_names = ['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
+        self.col_names = ['Timestamp (ms)', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
                           'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
+        self.datatypes = [int, float, float, float, float, float, int, float, int, float, float, int]
 
         self.dataset_path = f"{config.FOLDER_TO_SAVE}/binance"
         self.dataset_name = f"{self.currency_to_buy}{self.currency_to_sell}"
 
-    def get_data(self, start_time, end_time):
-        """Get Binance API exchange rates for all selected currencies. Operates in UTC timezone.
-        By default, it will try to continue from the latest available data point if the last saved timestamp is ahead
-        of the timestamp specified in the config, to save time instead of loading already available data."""
-
+    def scrape_data(self, start_time, end_time):
+        """
+        Get Binance API exchange rates for all selected currencies. Operates in UTC timezone.
+        """
         df = pd.DataFrame(columns=self.col_names)
 
         for currency_pair in self.currency_pairs:
-            start_time_datetime = time_helpers.timestamp_to_str(start_time, format="exact_time")
-            end_time_datetime = time_helpers.timestamp_to_str(end_time, format="exact_time")
             klines = self.client.get_exchange_rates(
                 currency_to_buy=self.currency_to_buy, currency_to_sell=self.currency_to_sell,
-                interval=self.interval, start_time=start_time_datetime, end_time=end_time_datetime)
+                interval=self.interval, start_time=start_time, end_time=end_time)
 
             temp_df = pd.DataFrame(klines, columns=self.col_names)
-            temp_df[config.MERGE_DATA_ON] = temp_df['Open time'].apply(partial(
-                time_helpers.timestamp_to_str, format='exact_time'))
+
+            # Setting the right data types to save later as metadata in parquet
+            temp_df = temp_df.astype({col_name: dtype for col_name, dtype in zip(self.col_names, self.datatypes)})
 
             # Rename the columns, except for merger column
             temp_df = temp_df.rename(columns={f'{col}': f'{currency_pair}_{col}' for col in temp_df.columns
-                                              if col != config.MERGE_DATA_ON})
-
-            if len(temp_df) == 0:
-                print(f'{currency_pair}_{self.interval} skipped.')
+                                              if col != 'Timestamp (ms)'})
 
             if len(df) == 0:
                 df = temp_df.copy()
             else:
-                df = df.merge(temp_df, how='outer', on=config.MERGE_DATA_ON)
+                df = df.merge(temp_df, how='outer', on='Timestamp (ms)')
 
-        # Creating general datetime column in seconds
-        df["datetime"] = df[f"{currency_pair}_Open time"]
+        # Getting the merger column
+        df[config.MERGE_DATA_ON] = pd.to_datetime(df['Timestamp (ms)'], unit='ms')
+
+        # Date column to partition by with parquet later
+        df['date'] = df[config.MERGE_DATA_ON].dt.date
 
         return df
 
-    def load_dataset(self, start_time, end_time):
-        full_dataset_name = f"{self.dataset_name}_{start_time}_{end_time}"
-        if self.dataset_not_stored(full_dataset_name):
-            to_store_dataset = self.get_data(start_time, end_time)
-            to_store_dataset.to_csv(f"{self.dataset_path}/{full_dataset_name}")
-            self.cache_dataset = to_store_dataset
+    def get_data(self, start_time, end_time):
+        """
+        Load the exchange data from Binance. Always scrapes data in production.
+        Tries to load data from disk in development. If it is not possible, scrapes the data and saves in parquet.
+        """
+        if not self.dev_run:  # Always scrape if in production
+            return self.scrape_data(start_time, end_time)
+        else:  # Try loading from disk if in development
+            self.load_from_disk(start_time, end_time)
+            return self.get_stored_data(end_time, start_time)
+
+    def load_from_disk(self, start_time, end_time):
+        if self.dataset_not_stored(start_time, end_time):
+            data = self.scrape_data(start_time, end_time)
+            table = pa.Table.from_pandas(data)  # Save the loaded data if in development/training
+            pq.write_to_dataset(table, root_path=f"{self.dataset_path}/{self.dataset_name}", partition_cols=['date'])
+            self.cache_dataset = data
         else:
-            self.cache_dataset = pd.read_csv(f"{self.dataset_path}/{full_dataset_name}")
+            self.cache_dataset = pq.read_table(source=f"{self.dataset_path}/{self.dataset_name}").to_pandas()
 
-        self.cache_dataset[config.MERGE_DATA_ON] = pd.to_datetime(self.cache_dataset[config.MERGE_DATA_ON],
-                                                                  infer_datetime_format=True)
+    def dataset_not_stored(self, start_time, end_time):
+        """
+        Checks availability of stored data on disk for selected start and end dates.
+        The data is available if requested start date >= saved start date and requested end date >= saved end date.
+        """
+        full_path = f"{self.dataset_path}/{self.dataset_name}"
+        if os.path.exists(full_path):
+            available_dates = os.listdir(full_path)
+            available_dates = [date.split('date=')[-1] for date in available_dates if not date.startswith('.')]
+            available_dates = [parser.parse(date).date() for date in available_dates]
 
-    def dataset_not_stored(self, full_dataset_name):
-        return not os.path.exists(f"{self.dataset_path}/{full_dataset_name}")
+            start_date = time_helpers.timestamp_to_datetime(start_time).date()
+            end_date = time_helpers.timestamp_to_datetime(end_time).date()
+
+            if not start_date >= min(available_dates) and end_date <= max(available_dates):
+                return True
+        else:
+            return True
 
     def get_stored_data(self, start_time, end_time):
+        """
+        Returns the exact slice of exchange data that is between start and end timestamp.
+        """
         data =  self.cache_dataset.loc[
-               (self.cache_dataset["datetime"] > start_time) & (
-                           self.cache_dataset["datetime"] <= end_time), :]
+               (self.cache_dataset["Timestamp (ms)"] > start_time) & (
+                           self.cache_dataset["Timestamp (ms)"] <= end_time), :]
 
         if len(data) == 0:
             raise RuntimeError("No more data")
